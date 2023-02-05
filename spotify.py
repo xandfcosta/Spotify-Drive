bd038450fbeb2723f12638d231bea49b150d30a0
Alexandre Fernandes Costa <49961419+xndcosta@users.noreply.github.com>
2023-02-05 09:24:58 -0300

    fix

--- spotify.py
+++ spotify.py
@@ -1,8 +1,8 @@
 from spotdl import Spotdl
 
 def download( music_link, threads ):
-    client_id = "21122f990feb4a08911a27cf2b8e722c"
-    client_secret = "81405a8fd862410e8bc71d89e996b234"
+    client_id = "CHANGE IT"
+    client_secret = "CHANGE IT"
 
     spotdl = Spotdl( client_id=client_id, client_secret=client_secret, threads= threads,output= "./musics" )
 
